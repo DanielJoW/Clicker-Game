2d53bbcb2a6a225a4436b969cfcc3dbc12e4f033
DanielJoW <46457263+DanielJoW@users.noreply.github.com>
2019-05-12 01:45:58 -0400

    Add files via upload
    Style Stuff: 
    Condensed counter into big button. Looks much better and makes page simpler. 
    Added a nice tiled background.
    Power Level and Invisible Clicker indicators are present prior to purchasing. Makes more sense I think.
    
    Bugs:
    Tooltips still suck. Somebody help.

--- clickscripts.js
+++ clickscripts.js
@@ -1,9 +1,12 @@
+/* Key Variables */
 
 var i = 0;
 var powerLevel = 1;
 var clickerFriends = 0;
 var powerCost = 100;
 var friendCost = 100;
+
+/*Naming Function with Easter Eggs */
 
 window.onload = function namePlayer(){
   var playerName = window.prompt("Please enter your name!");
@@ -13,15 +16,19 @@ window.onload = function namePlayer(){
   document.getElementById("playerlevel").innerHTML= "Power Level: " + powerLevel;
   document.getElementById("playertitle").innerHTML= playerName+"'s Big Clicker";
   }
-  else {
+  else if (playerName===""){
+  document.getElementById("playertitle").innerHTML= "Coward's Big Clicker";
+  }
+  else{
   document.getElementById("playertitle").innerHTML= playerName+"'s Big Clicker";
 }}
 
 /* Functions that must repeat constantly */
 
 setInterval(function(){
+  if (i > 0){
   document.getElementById("clicktotaltitle").innerHTML= "The Big Clicker ("+i+")";
-},5000);
+}},3000);
 
 setInterval(function(){
   document.getElementById("powerBuy").innerHTML= "Your points per click is equal to your Power Level. You need "+powerCost+" more points to power up!";
@@ -34,7 +41,7 @@ setInterval(function(){
 setInterval(function(){
   if (clickerFriends >=1){
     i += (1*clickerFriends);
-    document.getElementById("bigcounter").innerHTML=i;
+    document.getElementById("herobutton").innerHTML=i;
   }
 }, 1000);
 
@@ -42,23 +49,24 @@ setInterval(function(){
 
 function increment(){
   i += (1*powerLevel);
-  document.getElementById("bigcounter").innerHTML=i;
+  document.getElementById("herobutton").innerHTML=i;
 }
 
 function upgrade(){
   if (i>=powerCost){
-    document.getElementById("bigcounter").innerHTML= i -= powerCost;
+    document.getElementById("herobutton").innerHTML= i -= powerCost;
     powerLevel += 1;
     powerCost = Math.round(powerCost*1.5);
     document.getElementById("playerlevel").innerHTML= "Power Level: " + powerLevel;
-  }
+    }
   else {
-    alert("You need " + powerCost + " points to increase your Power Level!")
-  }}
+    alert("You need " + powerCost + " points to increase your Power Level!");
+  }
+}
 
 function buyClicker(){
   if (i>=friendCost){
-    document.getElementById("bigcounter").innerHTML= i -= friendCost;
+    document.getElementById("herobutton").innerHTML= i -= friendCost;
     clickerFriends += 1;
     friendCost = Math.round(friendCost*1.5);
     document.getElementById("clickeramount").innerHTML= "Invisible Clickers: " + clickerFriends;
@@ -66,4 +74,5 @@ function buyClicker(){
   else {
     alert("You need " + friendCost + " points to enlist a Clicker Friend!")
   }
+
 }
